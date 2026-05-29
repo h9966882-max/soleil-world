d1b90ae7283fac59a20eecda205bcc9c14307dd5
h9966882-max <h9966882@gmail.com>
2026-05-29 18:56:28 +0900

    supabaseClient.js を更新

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const SUPABASE_URL = "https://sclsozlhfaksfcgdggrg.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_N7gi3EbRyFFp_azw2H";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_N7gi3EbRyFFp_azw2HIakg_7qkm1oaT";
 
 export const supabase = createClient(
   SUPABASE_URL,
